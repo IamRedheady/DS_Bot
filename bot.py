04d0bb1769e692ae7361c750a9c4537eef974bce
Redheady <iam@redheady.ru>
2022-09-07 22:48:05 +0300

    Start Bot

--- bot.py
+++ bot.py
@@ -1,7 +1,7 @@
 import logging
 from aiogram import Bot, Dispatcher, executor, types
 
-API_TOKEN = 'BOT TOKEN HERE'
+API_TOKEN = '5508024822:AAELlrGvRzyh-Loe5--2e9OQjEeoz7PV0bQ'
 
 # Configure logging
 logging.basicConfig(level=logging.INFO)
@@ -9,18 +9,17 @@ logging.basicConfig(level=logging.INFO)
 bot = Bot(token=API_TOKEN)
 dp = Dispatcher(bot)
 
+
 @dp.message_handler(commands=['start', 'help'])
 async def send_welcome(message: types.Message):
     await message.reply("Hi!\nI'm EchoBot!\nPowered by aiogram.")
 
+
 @dp.message_handler()
 async def echo(message: types.Message):
-    # old style:
-    # await bot.send_message(message.chat.id, message.text)
 
     await message.answer(message.text)
 
 
 if __name__ == '__main__':
     executor.start_polling(dp, skip_updates=True)
-
